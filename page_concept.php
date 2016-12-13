f6ac2c37b688645c1bc3ef017beec8204b84ccd9
Marie CHARLES <marie.charles@hetic.net>
2016-12-13 23:35:57 +0100

    petite update index, concept, images et css

---
+++ page_concept.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>What'Sape - Concept</title>
+		<link rel="stylesheet" href="styles/styles.css">
+		<!--Polices-->
+		<link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300,400,700" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300,400,700|Raleway" rel="stylesheet">
+	</head>
+	<body>
+		<header>
+		</header>
+		<main>
+			<section class="definition">
+				<h2>Qu'est-ce que<br/><strong>What'Sape</strong><span>?</span></h2>
+				<article class="clearfix">
+					<img src="../img-layout/robeee.png" alt="Robe">
+					<div class="content clearfix">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi veniam quia magnam esse eius ex laborum possimus vel aliquam ducimus beatae distinctio, accusamus, dolore dignissimos explicabo, provident temporibus amet non. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, reiciendis, ratione! Adipisci velit quam quibusdam odit vitae soluta vel omnis, rem ut alias perspiciatis ipsam, impedit, assumenda repudiandae hic ratione. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, reiciendis, ratione! Adipisci velit quam quibusdam odit vitae soluta vel omnis, rem ut alias perspiciatis ipsam, impedit, assumenda repudiandae hic ratione!</p>
+						<ul>
+							<li><span class="vert">1</span>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+							<li><span class="violet">2</span>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+							<li><span class="bleu">3</span>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+						</ul>
+					</div>
+				</article>
+			</section>	
+		</main>
+		<footer>
+		 	<p>© WhatSape - Semaine intensive HETIC 2016. All Rights Reserved. Le présent site Web et l’ensemble de son contenu, y compris textes, images fixes ou animées, sont protégés par les droits d’auteur.<br/>Une production de: Kaouthar AZZOUNE, Déborah BAUD, Marie CHARLES, Romain FERRON, Antoine MASSELOT</p>
+		</footer>
+	</body>
+</html>
