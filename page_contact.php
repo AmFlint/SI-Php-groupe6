6f7c6af7197133327b7083f61d22dfd76d6c5ff4
Marie CHARLES <marie.charles@hetic.net>
2016-12-15 14:48:34 +0100

    page contact, style et les images

---
+++ page_contact.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>What'Sape</title>
+	<link rel="stylesheet" href="styles/styles.css">
+	<link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300,400,700|Raleway" rel="stylesheet">
+</head>
+<body>
+	<?php 
+	include_once "include/header.php";
+	?>
+	<section class="equipe">
+		<h2>Notre équipe</h2>
+		<p><span>N</span>ous sommes une équipe de cinq personnes.<br/> Nous avons tous participé à ce projet et nous nous sommes investis pour fournir un service de qualité et faciliter la vie de nos utilisateurs.</p>
+		<ul class="clearfix">
+			<li><img src="img-layout/romain.jpg" alt="photo">Romain</li>
+			<li><img src="img-layout/kaouthar.jpg" alt="photo">Kaouthar</li>
+			<li><img src="img-layout/antoine.jpg" alt="photo">Antoine</li>
+			<li><img src="img-layout/marie.jpg" alt="photo">Marie</li>
+			<li><img src="img-layout/deborah.jpg" alt="photo">Déborah</li>
+		</ul>
+	</section>
+	<?php 
+	include_once "include/footer.php";
+	?>
+</body>
+</html>
